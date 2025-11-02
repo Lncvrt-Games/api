44779b607b43d2dce8fca328639bd30ca99f79b1
Lncvrt <lncvrtreal@gmail.com>
2025-11-01 21:32:24 -0700

    bruh there

--- src/routes/launcher/loader/update-data.ts
+++ src/routes/launcher/loader/update-data.ts
@@ -1,6 +1,6 @@
 import { MySql2Database } from "drizzle-orm/mysql2";
 import { launcherUpdates } from "../../../lib/tables";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { jsonResponse } from "../../../lib/util";
 
 export async function handler(db: MySql2Database) {
@@ -12,7 +12,7 @@ export async function handler(db: MySql2Database) {
     })
         .from(launcherUpdates)
         .where(eq(launcherUpdates.hidden, false))
-        .orderBy(launcherUpdates.place)
+        .orderBy(desc(launcherUpdates.place))
         .limit(1)
         .execute()
 
