089421066b6a45ce4b59af86ecc2eda78b42e8db
Lncvrt <lncvrtreal@gmail.com>
2026-02-10 12:27:55 -0700

    Set the beta version name to `26.1-beta`

--- src/info/general.ts
+++ src/info/general.ts
@@ -1,5 +1,5 @@
 export const latestVersion = '26.1'
-export const latestBetaVersion = latestVersion
+export const latestBetaVersion = '26.1-beta1'
 export const latestLauncherLoaderVersion = '1.0.1'
 export const allowedVersions = [latestVersion, latestBetaVersion]
 export const allowedDatabaseVersions = [latestVersion, latestBetaVersion]
